d2ddb7bd2f2d086eb6ad300a4f9e60d86a798702
besandr <grandeus@gmail.com>
2019-06-14 23:39:05 +0300

    removed redundant for this branch classes

--- src/main/java/common/post/model/office/PostOfficeObserver.java
+++
@@ -1,9 +0,0 @@
-package common.post.model.office;
-
-import common.post.model.product.PublishingProduct;
-
-public interface PostOfficeObserver {
-
-    void update(PublishingProduct product);
-
-}
--- src/main/java/common/post/model/publishing/PublishingObserver.java
+++
@@ -1,9 +0,0 @@
-package common.post.model.publishing;
-
-import common.post.model.product.PublishingProduct;
-
-public interface PublishingObserver {
-
-    void update(PublishingProduct product);
-
-}
