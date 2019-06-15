b5dcabc0f54273a9c7c15694c196643d4de77568
besandr <grandeus@gmail.com>
2019-06-15 15:16:28 +0300

    Implemented DetergentDrawerCompartment class

---
+++ src/main/Java/individual/model/DetergentDrawerCompartment.java
@@ -0,0 +1,14 @@
+package individual.model;
+
+public class DetergentDrawerCompartment {
+
+    private boolean isFilled;
+
+    public boolean isFilled() {
+        return isFilled;
+    }
+
+    public void setFilled(boolean filled) {
+        isFilled = filled;
+    }
+}
