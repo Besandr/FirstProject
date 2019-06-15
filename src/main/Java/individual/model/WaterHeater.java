e4f0e2ac7e2410a1aca3a6423101bac416d43c6c
besandr <grandeus@gmail.com>
2019-06-15 16:15:21 +0300

    Implemented TemperatureSensor class

--- src/main/Java/individual/model/WaterHeater.java
+++ src/main/Java/individual/model/WaterHeater.java
@@ -1,4 +1,27 @@
 package individual.model;
 
-public class WaterHeater {
+class WaterHeater {
+
+    private TemperatureSensor sensor;
+
+    WaterHeater(TemperatureSensor sensor){
+        this.sensor = sensor;
+    }
+
+    void heat(int targetTemperature) {
+
+        int currentTemp = sensor.getCurrentTemperature();
+
+        while (currentTemp < targetTemperature) {
+
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {}
+
+            currentTemp++;
+        }
+
+        sensor.setCurrentTemperature(currentTemp);
+    }
+
 }
