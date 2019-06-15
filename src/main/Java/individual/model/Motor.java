08437ac8c5c2342f3ff29467a9ead72a216513ff
besandr <grandeus@gmail.com>
2019-06-15 15:10:40 +0300

    Implemented Motor class

--- src/main/Java/individual/model/Motor.java
+++ src/main/Java/individual/model/Motor.java
@@ -1,4 +1,37 @@
 package individual.model;
 
 public class Motor {
+
+    public enum RotatingType {
+        SLOW(20), NORMAL(60), DRYING(800);
+
+        private int motorSpeed;
+
+        RotatingType(int motorSpeed) {
+            this.motorSpeed = motorSpeed;
+        }
+
+        public int getMotorSpeed() {
+            return motorSpeed;
+        }
+    }
+
+    int currentMotorSpeed;
+    boolean isMotorOn;
+
+    public int getCurrentMotorSpeed() {
+        return currentMotorSpeed;
+    }
+
+    public void setCurrentMotorSpeed(RotatingType type) {
+        currentMotorSpeed = type.getMotorSpeed();
+    }
+
+    public boolean isMotorOn() {
+        return isMotorOn;
+    }
+
+    public void setIsMotorOn(boolean motorOn) {
+        isMotorOn = motorOn;
+    }
 }
