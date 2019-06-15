4ebc72b2209e142f0a09e7b5c38c5f830fc3a95c
besandr <grandeus@gmail.com>
2019-06-15 15:18:26 +0300

    Implemented DetergentDrawer class

--- src/main/Java/individual/model/DetergentDrawer.java
+++ src/main/Java/individual/model/DetergentDrawer.java
@@ -1,4 +1,29 @@
 package individual.model;
 
+import java.util.HashMap;
+
 public class DetergentDrawer {
+
+    public enum DetergentType {PRE_WASH, MAIN_WASH, SOFTENER}
+
+    private HashMap<DetergentType, DetergentDrawerCompartment> compartments;
+
+    DetergentDrawer() {
+        compartments = new HashMap<>();
+        compartments.put(DetergentType.PRE_WASH, new DetergentDrawerCompartment());
+        compartments.put(DetergentType.MAIN_WASH, new DetergentDrawerCompartment());
+        compartments.put(DetergentType.SOFTENER, new DetergentDrawerCompartment());
+    }
+
+    void loadDetergent(DetergentType type) {
+        compartments.get(type).setFilled(true);
+    }
+
+    void useDetergent(DetergentType type) {
+        compartments.get(type).setFilled(false);
+    }
+
+    boolean hasDetergent(DetergentType type) {
+        return compartments.get(type).isFilled();
+    }
 }
