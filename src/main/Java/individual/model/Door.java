642c455176ac1fed7e6503452392106d2d85ec50
besandr <grandeus@gmail.com>
2019-06-15 14:55:48 +0300

    Implemented Door and DoorTest classes

--- src/main/Java/individual/model/Door.java
+++ src/main/Java/individual/model/Door.java
@@ -1,4 +1,41 @@
 package individual.model;
 
 public class Door {
+
+    private boolean isOpened;
+    private boolean isLocked;
+
+    public boolean isOpened() {
+        return isOpened;
+    }
+
+    public boolean setOpened() {
+        if (isLocked) {
+            return false;
+        } else {
+            isOpened = true;
+            return true;
+        }
+    }
+
+    public void setClosed(){
+        isOpened = false;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public boolean setLocked() {
+        if (isOpened) {
+            return false;
+        } else {
+            isLocked = true;
+            return true;
+        }
+    }
+
+    public void setUnlocked(){
+        isLocked = false;
+    }
 }
